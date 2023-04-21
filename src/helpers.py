338dac9dffe3eb4327786cc100ab3a0c335ec098
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-04-21 07:26:54 -0700

    class 1 - implement bencode decode

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Tuple, Union
 import builtins
 
 
@@ -35,3 +35,59 @@ def encode_item(item: Item) -> str:
 
         case _:
             raise Exception(f"Exhaustive switch error in encoding {item}.")
+
+
+def decode_item(string: str) -> Optional[Item]:
+    def closure(string: str) -> Tuple[Optional[Item], str]:
+        if string == "":
+            return None, ""
+
+        elif string[0].isdigit():
+            counter = 1
+
+            while string[counter].isdigit():
+                counter += 1
+
+            length = int(string[:counter])
+
+            if length == 0:
+                return "", ""
+
+            assert string[counter] == ":"
+            return (
+                string[counter + 1 : counter + 1 + length],
+                string[counter + 1 + length :],
+            )
+
+        elif string.startswith("i"):
+            counter = 1
+
+            if string[counter] == "-":
+                counter += 1
+
+            while string[counter].isdigit():
+                counter += 1
+
+            assert string[counter] == "e"
+            return int(string[1:counter]), string[counter + 1 :]
+
+        elif string[0] in {"l", "d"}:
+            items = []
+            rest = string[1:]
+
+            while not rest.startswith("e"):
+                item, rest = closure(rest)
+                items.append(item)
+
+            assert rest[0] == "e"
+            rest = rest[1:]
+
+            if string.startswith("l"):
+                return items, rest
+
+            return {k: v for k, v in zip(items[::2], items[1::2])}, rest
+
+        else:
+            raise Exception(f"Cannot decode {string}.")
+
+    return closure(string)[0]
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,7 +1,7 @@
 import helpers
 
 
-def test_encode_items():
+def test_encode_item():
     assert helpers.encode_item(None) == ""
     assert helpers.encode_item("") == "0:"
     assert helpers.encode_item([]) == "le"
@@ -21,3 +21,22 @@ def test_encode_items():
     assert helpers.encode_item({"foo": [1]}) == "d3:fooli1eee"
     assert helpers.encode_item([{"foo": 1}]) == "ld3:fooi1eee"
     assert helpers.encode_item({"foo": {"bar": "baz"}}) == "d3:food3:bar3:bazee"
+
+
+def test_decode_item():
+    assert helpers.decode_item("") == None
+    assert helpers.decode_item("0:") == ""
+    assert helpers.decode_item("le") == []
+    assert helpers.decode_item("de") == {}
+    assert helpers.decode_item("i1e") == 1
+    assert helpers.decode_item("i-1e") == -1
+    assert helpers.decode_item("i0e") == 0
+    assert helpers.decode_item("3:foo") == "foo"
+    assert helpers.decode_item("li1ee") == [1]
+    assert helpers.decode_item("l3:fooe") == ["foo"]
+    assert helpers.decode_item("li1e3:fooe") == [1, "foo"]
+    assert helpers.decode_item("d3:fooi1ee") == {"foo": 1}
+    assert helpers.decode_item("li1el3:fooee") == [1, ["foo"]]
+    assert helpers.decode_item("d3:fooli1eee") == {"foo": [1]}
+    assert helpers.decode_item("ld3:fooi1eee") == [{"foo": 1}]
+    assert helpers.decode_item("d3:food3:bar3:bazee") == {"foo": {"bar": "baz"}}
